c780904a2ceaa8d61686fb8298fda4aa1a9db97f
Mailhot <fmailhot@maxidrill.com>
2024-04-12 09:19:56 -0400

    adding a recursive element, to add multiple elements to an element

--- drafter.py
+++ drafter.py
@@ -1,1 +1,14 @@
+
+# draw Square in Python Turtle
 import turtle
+  
+t = turtle.Turtle()
+ 
+s = int(input("Enter the length of the side of square: "))
+
+t.penup()
+t.goto(0,-300)
+t.pendown()
+for _ in range(4):
+  t.forward(s) # Forward turtle by s units
+  t.left(90) # Turn turtle by 90 degree
